d86081d85dfb59b44a25c42c3b645b014e726062
MatteoMorisoli <matteo.morisoli@usi.ch>
2018-02-19 10:58:19 +0100

    Created initial project

---
+++ MultiTreeRouting/MultiTreeRouting/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MultiTreeRouting
+//
+//  Created by Matteo Morisoli on 19.02.18.
+//  Copyright © 2018 MatteoMorisoli. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
